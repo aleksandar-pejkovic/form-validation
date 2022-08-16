b00a456d382385d65dce4c6570628db22ba03d2d
pejko89 <pejko89.ap@gmail.com>
2022-08-16 17:23:04 +0200

    final commit

--- script.js
+++ script.js
@@ -1,73 +1,110 @@
 let inputs = document.querySelectorAll('input')
-let errors = {
-    "name": [],
-    "username": [],
-    "email": [],
-    "password": [],
-    "retypePassword": [],
-}
+let allFieldsAreValid = false
 
-inputs.forEach(element => {
-    element.addEventListener('change', e => {
-        let currentInput = e.target
-        let inputValue = currentInput.value
-        let inputName = currentInput.getAttribute('name')
+inputs.forEach(input => {
+    input.addEventListener('change', () => {
+        let inputValue = input.value
+        let inputName = input.getAttribute('name')
+        let nextSibling = input.nextElementSibling
 
-
-        if (inputValue.length > 4) {
-
-            switch (inputName) {
-                case 'name':
-                    let validation = inputValue.trim()
-                    validation = validation.split(" ")
-                    if (validation.length < 2) {
-                        errors[inputName].push('Enter both first name and last name!')
-                    }
-                    break
-                case 'email':
-                    if (!validateEmail(inputValue)) {
-                        errors[inputName].push('Email is invalid')
-                    }
-                    break
-            }
-
-
-        } else {
-            errors[inputName] = ['Too short']
-        }
-
-        console.log(`${inputName} = ${inputValue}`)
-
-        populateErrors()
+        validate(nextSibling, inputValue, inputName)
     })
 })
 
-const populateErrors = () => {
+let submit = document.querySelector('#submit')
 
-    for (let elem of document.querySelectorAll('ul')) {
-        elem.remove()
+document.addEventListener('keypress', (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault()
+        submit.click()
+    }
+})
+
+submit.addEventListener('click', e => {
+
+    if (areEmpty(inputs)) {
+        allFieldsAreValid = false
     }
 
-    for (let key of Object.keys(errors)) {
-        let input = document.querySelector(`input[name="${key}"]`)
-        let parenteElement = input.parentElement
-        let errorsElement = document.createElement('ul')
-        parenteElement.appendChild(errorsElement)
-
-
-        errors[key].forEach(error => {
-            let li = document.createElement('li')
-            li.innerText = error
-
-            errorsElement.appendChild(li)
-        })
-
+    if (!allFieldsAreValid) {
+        alert('All fields must be valid!')
+        e.preventDefault()
+    } else {
+        confirm('Everything is OK! Proceed with registration?')
     }
+
+})
+
+
+function validate(nextSibling, inputValue, inputName) {
+
+    allFieldsAreValid = true
+    printError(nextSibling, "")
+
+    if (inputValue.length < 6) {
+        allFieldsAreValid = false
+        printError(nextSibling, "Input too short! ")
+    }
+
+    switch (inputName) {
+        case 'name':
+            let validation = inputValue.trim()
+            validation = validation.split(" ")
+            if (validation.length < 2) {
+                allFieldsAreValid = false
+                printError(nextSibling, "Enter both first and last name!")
+            }
+            break
+        case 'email':
+            if (!validateEmail(inputValue)) {
+                allFieldsAreValid = false
+                printError(nextSibling, "Email invalid!")
+            }
+            break
+        case 'password':
+            if (!validatePassword(inputValue)) {
+                allFieldsAreValid = false
+                printError(nextSibling, `Password must:\n
+                Be at least 8 characters long\n
+                Include both lower and upper case characters\n
+                Include at least one number and symbol`)
+            }
+            break
+        case 'retypePassword':
+            let password = document.querySelector('#password').value
+
+            if (password != inputValue) {
+                allFieldsAreValid = false
+                printError(nextSibling, "Plese retype password correctly!")
+            }
+            break
+    }
+
 
 }
 
-const validateEmail = email => {
-    if ('/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/'.test(eamil)) {
+function areEmpty(inputs) {
+    inputs.forEach(e => {
+        if (e.value === "") {
+            allFieldsAreValid = false
+        }
+    })
+}
+
+function printError(nextSibling, msg) {
+    nextSibling.innerText = msg
+}
+
+function validatePassword(inputValue) {
+    if (/^(?=.*\d)(?=.*[.?!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(inputValue)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function validateEmail(inputValue) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputValue)) {
         return true
     } else {
         return false
